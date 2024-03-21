1ceb0d031adcc40e80bb350e74e7cf9da08cdc20
parthd1631 <87785093+parthd1631@users.noreply.github.com>
2024-03-21 12:33:10 -0400

    first commit

---
+++ src/forensic/ForensicAnalysis.java
@@ -0,0 +1,330 @@
+package forensic;
+
+import javax.crypto.Mac;
+
+/**
+ * This class represents a forensic analysis system that manages DNA data using
+ * BSTs.
+ * Contains methods to create, read, update, delete, and flag profiles.
+ * 
+ * @author Kal Pandit
+ */
+public class ForensicAnalysis {
+
+    private TreeNode treeRoot;            // BST's root
+    private String firstUnknownSequence;
+    private String secondUnknownSequence;
+
+    public ForensicAnalysis () {
+        treeRoot = null;
+        firstUnknownSequence = null;
+        secondUnknownSequence = null;
+    }
+
+    /**
+     * Builds a simplified forensic analysis database as a BST and populates unknown sequences.
+     * The input file is formatted as follows:
+     * 1. one line containing the number of people in the database, say p
+     * 2. one line containing first unknown sequence
+     * 3. one line containing second unknown sequence
+     * 2. for each person (p), this method:
+     * - reads the person's name
+     * - calls buildSingleProfile to return a single profile.
+     * - calls insertPerson on the profile built to insert into BST.
+     *      Use the BST insertion algorithm from class to insert.
+     * 
+     * DO NOT EDIT this method, IMPLEMENT buildSingleProfile and insertPerson.
+     * 
+     * @param filename the name of the file to read from
+     */
+    public void buildTree(String filename) {
+        // DO NOT EDIT THIS CODE
+        StdIn.setFile(filename); // DO NOT remove this line
+
+        // Reads unknown sequences
+        String sequence1 = StdIn.readLine();
+        firstUnknownSequence = sequence1;
+        String sequence2 = StdIn.readLine();
+        secondUnknownSequence = sequence2;
+        
+        int numberOfPeople = Integer.parseInt(StdIn.readLine()); 
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            // Reads name, count of STRs
+            String fname = StdIn.readString();
+            String lname = StdIn.readString();
+            String fullName = lname + ", " + fname;
+            // Calls buildSingleProfile to create
+            Profile profileToAdd = createSingleProfile();
+            // Calls insertPerson on that profile: inserts a key-value pair (name, profile)
+            insertPerson(fullName, profileToAdd);
+        }
+    }
+
+    /** 
+     * Reads ONE profile from input file and returns a new Profile.
+     * Do not add a StdIn.setFile statement, that is done for you in buildTree.
+    */
+    public Profile createSingleProfile() {
+
+        // WRITE YOUR CODE HERE
+        int s = StdIn.readInt(); 
+        STR [] STRs = new STR[s]; 
+
+        for(int i = 0; i <STRs.length; i++) {
+            String name = StdIn.readString(); 
+            int occurrences = StdIn.readInt(); 
+            STRs[i] = new STR(name, occurrences); 
+        }
+
+
+        
+        return new Profile(STRs); // update this line
+    }
+
+    /**
+     * Inserts a node with a new (key, value) pair into
+     * the binary search tree rooted at treeRoot.
+     * 
+     * Names are the keys, Profiles are the values.
+     * USE the compareTo method on keys.
+     * 
+     * @param newProfile the profile to be inserted
+     */
+
+
+    private TreeNode insertion(TreeNode n, String name, Profile profile) {
+        if(n==null) {
+            return new TreeNode(name, profile, null, null); 
+        }
+
+        int valueToCompare = name.compareTo(n.getName());
+
+
+        if(valueToCompare < 0) {
+            n.setLeft(insertion(n.getLeft(), name, profile));
+        }
+
+        
+        else if(valueToCompare>0) {
+            n.setRight(insertion(n.getRight(), name, profile));
+        }
+
+
+        return n; 
+    }
+
+
+
+
+    
+    public void insertPerson(String name, Profile newProfile) {
+
+        // WRITE YOUR CODE HERE
+        treeRoot = insertion(treeRoot, name, newProfile); 
+
+
+
+    }
+
+    /**
+     * Finds the number of profiles in the BST whose interest status matches
+     * isOfInterest.
+     *
+     * @param isOfInterest the search mode: whether we are searching for unmarked or
+     *                     marked profiles. true if yes, false otherwise
+     * @return the number of profiles according to the search mode marked
+     */
+    
+
+    private int matchingProfile(TreeNode root, boolean val) {
+
+        int count = 0; 
+        if(root != null) {
+            matchingProfile(root.getLeft(), val); 
+            if(root.getProfile().getMarkedStatus() == false && root.getProfile().getMarkedStatus() == val) {
+                count++; 
+            }
+
+            else if(root.getProfile().getMarkedStatus() == true && root.getProfile().getMarkedStatus() == val) {
+                count++; 
+            }
+            matchingProfile(root.getRight(), val);
+        }
+
+        return count;
+    }
+
+
+
+
+    public int getMatchingProfileCount(boolean isOfInterest) {
+        
+        // WRITE YOUR CODE HERE
+        return matchingProfile(treeRoot, isOfInterest); 
+
+        }
+
+    /**
+     * Helper method that counts the # of STR occurrences in a sequence.
+     * Provided method - DO NOT UPDATE.
+     * 
+     * @param sequence the sequence to search
+     * @param STR      the STR to count occurrences of
+     * @return the number of times STR appears in sequence
+     */
+    private int numberOfOccurrences(String sequence, String STR) {
+        
+        // DO NOT EDIT THIS CODE
+        
+        int repeats = 0;
+        // STRs can't be greater than a sequence
+        if (STR.length() > sequence.length())
+            return 0;
+        
+            // indexOf returns the first index of STR in sequence, -1 if not found
+        int lastOccurrence = sequence.indexOf(STR);
+        
+        while (lastOccurrence != -1) {
+            repeats++;
+            // Move start index beyond the last found occurrence
+            lastOccurrence = sequence.indexOf(STR, lastOccurrence + STR.length());
+        }
+        return repeats;
+    }
+
+    /**
+     * Traverses the BST at treeRoot to mark profiles if:
+     * - For each STR in profile STRs: at least half of STR occurrences match (round
+     * UP)
+     * - If occurrences THROUGHOUT DNA (first + second sequence combined) matches
+     * occurrences, add a match
+     */
+
+    private void traversal(TreeNode root) {
+
+        if(root == null) {
+            return; 
+        }
+
+        if(root!=null) {
+            traversal(root.getLeft());
+
+        int count = 0; 
+        STR [] STRs = root.getProfile().getStrs(); 
+
+        for(int i = 0; i < STRs.length; i++) {
+            if(numberOfOccurrences(firstUnknownSequence, STRs[i].getStrString()) + numberOfOccurrences(secondUnknownSequence, STRs[i].getStrString()) == STRs[i].getOccurrences()) {
+                count++; 
+            }
+        }
+
+
+        if(Math.ceil(STRs.length/2.0)<=count) {
+            root.getProfile().setInterestStatus(true);
+            }
+        
+            traversal(root.getRight());
+        }
+    }
+
+
+    public void flagProfilesOfInterest() {
+
+        // WRITE YOUR CODE HERE
+        traversal(treeRoot);
+    }
+
+    /**
+     * Uses a level-order traversal to populate an array of unmarked Strings representing unmarked people's names.
+     * - USE the getMatchingProfileCount method to get the resulting array length.
+     * - USE the provided Queue class to investigate a node and enqueue its
+     * neighbors.
+     * 
+     * @return the array of unmarked people
+     */
+    public String[] getUnmarkedPeople() {
+
+        // WRITE YOUR CODE HERE
+        return null; // update this line
+    }
+
+    /**
+     * Removes a SINGLE node from the BST rooted at treeRoot, given a full name (Last, First)
+     * This is similar to the BST delete we have seen in class.
+     * 
+     * If a profile containing fullName doesn't exist, do nothing.
+     * You may assume that all names are distinct.
+     * 
+     * @param fullName the full name of the person to delete
+     */
+    public void removePerson(String fullName) {
+        // WRITE YOUR CODE HERE
+        
+    }
+
+    /**
+     * Clean up the tree by using previously written methods to remove unmarked
+     * profiles.
+     * Requires the use of getUnmarkedPeople and removePerson.
+     */
+    public void cleanupTree() {
+        // WRITE YOUR CODE HERE
+
+    }
+
+    /**
+     * Gets the root of the binary search tree.
+     *
+     * @return The root of the binary search tree.
+     */
+    public TreeNode getTreeRoot() {
+        return treeRoot;
+    }
+
+    /**
+     * Sets the root of the binary search tree.
+     *
+     * @param newRoot The new root of the binary search tree.
+     */
+    public void setTreeRoot(TreeNode newRoot) {
+        treeRoot = newRoot;
+    }
+
+    /**
+     * Gets the first unknown sequence.
+     * 
+     * @return the first unknown sequence.
+     */
+    public String getFirstUnknownSequence() {
+        return firstUnknownSequence;
+    }
+
+    /**
+     * Sets the first unknown sequence.
+     * 
+     * @param newFirst the value to set.
+     */
+    public void setFirstUnknownSequence(String newFirst) {
+        firstUnknownSequence = newFirst;
+    }
+
+    /**
+     * Gets the second unknown sequence.
+     * 
+     * @return the second unknown sequence.
+     */
+    public String getSecondUnknownSequence() {
+        return secondUnknownSequence;
+    }
+
+    /**
+     * Sets the second unknown sequence.
+     * 
+     * @param newSecond the value to set.
+     */
+    public void setSecondUnknownSequence(String newSecond) {
+        secondUnknownSequence = newSecond;
+    }
+
+}
